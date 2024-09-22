9c0db399549f4868cebc0239248ba176a56628f6
Agafina <98034773+Agafina@users.noreply.github.com>
2024-09-23 00:29:12 +0100

    first commit

---
+++ app/layout.js
@@ -0,0 +1,20 @@
+
+import Navbar from "@/components/Navbar";
+import "./globals.css";
+
+
+export const metadata = {
+  title: "Todo Next app",
+  description: "Created by Agafina"
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <Navbar />
+       <div>{children}</div>
+      </body>
+    </html>
+  );
+}
---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <form className="flex items-start flex-col max-w-[600px] w-[80%] gap-2 mt-24 px-2 mx-auto">
+      <input type="text" placeholder="Enter Title" className="w-full px-3 py-2 border-2"  />
+    </form>
+  );
+}
+ 
